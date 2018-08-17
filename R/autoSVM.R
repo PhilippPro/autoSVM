4389a94c25f6ba267ad54c766d59986c7d8bfd91
PhilippPro <philipp_probst@gmx.de>
2018-08-17 16:56:56 +0200

    first real commit

---
+++ R/autoSVM.R
@@ -0,0 +1,4 @@
+autoSVM = function(task, measure = NULL, iters = 70, iters.warmup = 30, num.threads = NULL, num.trees = 1000, 
+  parameters = list(replace = FALSE, respect.unordered.factors = "order"), 
+  tune.parameters = c("mtry", "min.node.size", "sample.fraction"), save.file.path = NULL,
+  build.final.model = TRUE, show.info = getOption("mlrMBO.show.info", TRUE)) 
